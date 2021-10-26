b22929bbc9cbba97753fc3d53ca05d5b3d13f5e9
nineuagustineu09 <nineuagustineu09@gmail.com>
2021-10-26 19:51:51 +0700

    persiapan dalam membuat aplikasi pustaka booking

---
+++ pustaka.sql
@@ -0,0 +1,179 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 26 Okt 2021 pada 14.30
+-- Versi server: 10.4.18-MariaDB
+-- Versi PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pustaka`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku`
+--
+
+CREATE TABLE `buku` (
+  `id` int(11) NOT NULL,
+  `judul_buku` varchar(128) CHARACTER SET latin1 NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `pengarang` varchar(64) CHARACTER SET latin1 NOT NULL,
+  `penerbit` varchar(64) CHARACTER SET latin1 NOT NULL,
+  `tahun_terbit` year(4) NOT NULL,
+  `isbn` varchar(64) CHARACTER SET latin1 NOT NULL,
+  `stok` int(11) NOT NULL,
+  `dipinjam` int(11) NOT NULL,
+  `dibooking` int(11) NOT NULL,
+  `image` varchar(256) CHARACTER SET latin1 DEFAULT 'book-default-cover.jpg'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `buku`
+--
+
+INSERT INTO `buku` (`id`, `judul_buku`, `id_kategori`, `pengarang`, `penerbit`, `tahun_terbit`, `isbn`, `stok`, `dipinjam`, `dibooking`, `image`) VALUES
+(1, 'statistika dengan program komputer', 1, 'Ahmaad kholiqul amin', 'Deep publish', 2014, '9786022809432', 6, 1, 1, 'img1557402455.jpg'),
+(2, 'mudah belajar komputer untuk anak', 1, 'Bambang Agus Setiawan', 'Huta Media', 2014, '9786025118500', 5, 3, 1, 'img1557402397.jpg'),
+(5, 'php komplet', 1, 'Jubilee', 'Elex Media Komputindo', 2017, '83467535447', 5, 1, 1, 'img1555522493.jpg'),
+(10, 'Dektif conan Ep 200', 9, 'Okigawa sasuke', 'Cultura', 2016, '874387583987', 5, 0, 0, 'img1557401465.jpg'),
+(14, 'bahasa indonesia', 2, 'Umri nur\'aini dan indriyani \r\n', 'pusat perbukuan', 2015, '757254724884', 3, 0, 0, 'img1557402703.jpg'),
+(15, 'komunikasi lintas budaya', 5, 'dr.dedy kurnia', 'published', 2015, '878674646488', 5, 0, 0, 'img1557403502.jpg'),
+(16, 'kolaborasi codeigniter dan ajax dalam perancangan...', 1, 'anton subagia', 'elex media komputindo', 2017, '43345356577', 5, 0, 0, 'book-default-cover.jpg'),
+(17, 'kolaborasi codeigniter dan ajax dalam perancangan...', 1, 'anton subagia', 'elex media komputindo', 2017, '43345356577', 5, 0, 0, 'book-default-cover.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(5) NOT NULL,
+  `nama_kategori` varchar(45) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
+(1, 'komputer'),
+(2, 'bahasa'),
+(3, 'sains'),
+(4, 'hobby'),
+(5, 'komunikasi'),
+(6, 'hukum'),
+(7, 'agama'),
+(8, 'populer'),
+(9, 'komik');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `role`
+--
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `role` varchar(128) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `role`
+--
+
+INSERT INTO `role` (`id`, `role`) VALUES
+(1, 'admministrator'),
+(2, 'member');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(128) CHARACTER SET latin1 NOT NULL,
+  `email` varchar(128) CHARACTER SET latin1 NOT NULL,
+  `image` varchar(128) CHARACTER SET latin1 NOT NULL,
+  `password` varchar(256) CHARACTER SET latin1 NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `is_active` int(1) NOT NULL,
+  `tanggal_input` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
